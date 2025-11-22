fae16d08d498ff03d386fb54fe4929418a63cda3
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-22 21:38:17 +0200

    user component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header.component";
+import { HeaderComponent } from "./header/header.component";
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, User],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
