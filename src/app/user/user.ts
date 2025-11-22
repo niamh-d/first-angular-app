42b86aa23c2bab1449c84ba29b85019d71e2f62b
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-22 21:54:59 +0200

    rendering user image and user name

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.html',
   styleUrl: './user.css',
 })
 export class User {
+  selectedUser = DUMMY_USERS[randomIndex];
 
+  get imagePath() {
+    return 'assets/users/' + this.selectedUser.avatar;
+  }
+
+  get altDescription() {
+    return this.selectedUser.name + ' profile photo';
+  }
 }
