4a6df577a7fbef44797d5f9e1f5a18601869c26f
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-23 20:47:08 +0200

    add new task dialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "./header/header.component";
-import { User } from "./user/user";
+import { HeaderComponent } from './header/header.component';
+import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
 import { Tasks } from './tasks/tasks';
 
@@ -16,7 +16,7 @@ export class AppComponent {
   selectedUserId?: string;
 
   get selectedUser() {
-    return this.users.find(user => user.id === this.selectedUserId);
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
   onSelectUser(userId: string) {
--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,11 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Task } from './task/task';
 
 import { dummyTasks } from '../dummy-tasks';
+import { AddTask } from './add-task/add-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, AddTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
@@ -13,6 +14,7 @@ export class Tasks {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) name!: string;
   listOfTasks = dummyTasks;
+  showAddTaskDialog = false;
 
   get selectedUserTasks() {
     return this.listOfTasks.filter((task) => task.userId === this.id);
@@ -20,5 +22,9 @@ export class Tasks {
 
   onCompleteTask(taskId: string) {
     this.listOfTasks = this.listOfTasks.filter((task) => task.id !== taskId);
+  }
+
+  onAddNewTask() {
+    this.showAddTaskDialog = true;
   }
 }
