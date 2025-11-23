5b8366b26767a8dac4a867f2cbaeda3fbac8c0fc
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-23 13:55:19 +0200

    using signals in user component

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, computed } from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy-users';
 
@@ -10,13 +10,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  selectedUser = DUMMY_USERS[randomIndex];
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => {
+    return 'assets/users/' + this.selectedUser().avatar;
+  });
+  altDescription = computed(() => {
+    return this.selectedUser().name + ' profile photo';
+  });
 
-  get imagePath() {
-    return 'assets/users/' + this.selectedUser.avatar;
-  }
-
-  get altDescription() {
-    return this.selectedUser.name + ' profile photo';
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
