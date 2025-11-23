b7ff4cf651f4fd42594435cc990427a444219b7f
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-23 16:16:22 +0200

    dynamic list of users

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,5 +1,11 @@
 import { Component, computed, EventEmitter, Input, Output} from '@angular/core';
 
+interface IUser {
+  id: string;
+  name: string;
+  avatar: string;
+}
+
 @Component({
   selector: 'app-user',
   templateUrl: './user.html',
@@ -7,20 +13,18 @@ import { Component, computed, EventEmitter, Input, Output} from '@angular/core';
 })
 export class User {
 
-  @Input({ required: true }) id!: string;
-  @Input({ required: true }) avatar!: string;
-  @Input({ required: true }) name!: string;
+  @Input() user!: IUser;
   @Output() select = new EventEmitter<string>();
 
   get imagePath() {
-    return 'assets/users/' + this.avatar;
+    return 'assets/users/' + this.user.avatar;
   }
 
   get altDescription() {
-    return this.name + "'s avatar";
+    return this.user.name + "'s avatar";
   }
 
   onSelectUser() {
-    this.select.emit(this.id);
+    this.select.emit(this.user.id);
   }
 }
