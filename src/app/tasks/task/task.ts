c95864378fffd8e0208f2b04582cf30532964fb0
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-25 21:11:18 +0200

    date pipe for formatting due date

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ITask } from '../task/task.model';
 import { Card } from '../../shared/card/card';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css',
 })
