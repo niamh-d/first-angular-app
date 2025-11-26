45d3fb3649d6120f8ee1595ab9d6d8f8c6c6e349
Niamh <85398097+niamh-d@users.noreply.github.com>
2025-11-26 18:48:29 +0200

    migrated app component to use module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { User } from './user/user';
 import { DUMMY_USERS } from './dummy-users';
-import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [HeaderComponent, User, Tasks],
+  standalone: false,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
